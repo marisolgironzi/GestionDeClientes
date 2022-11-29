8869d28bbcdb09a9cd2a9e1abc7c374500dd271a
Marisol <gironzimarisol@gmail.com>
2022-11-29 01:50:13 -0300

    rutas modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarComponent } from './moduloEmpleados/agregar/agregar.component';
+import { EditarComponent } from './moduloEmpleados/editar/editar.component';
+import { ListarComponent } from './moduloEmpleados/listar/listar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch:'full', redirectTo:'listarEmpleados'},
+  {path:'listarEmpleados', component:ListarComponent},
+  {path:'agregarEmpleado', component:AgregarComponent},
+  {path:'editarEmpleado/:id', component:EditarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
